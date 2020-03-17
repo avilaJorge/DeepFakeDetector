f4db72644e98349fd7f4c6f1bd7fececba167353
Jorge Avila <avilajorge314@gmail.com>
2020-03-17 10:17:33 +0000

    Added script for downloading all hdf5 datasets

---
+++ download_all_hdf5.sh
@@ -0,0 +1,6 @@
+wget https://storage.googleapis.com/img-hosting-bucket/Faces_HQ.hdf5
+wget https://storage.googleapis.com/img-hosting-bucket/LSUN.hdf5
+wget https://storage.googleapis.com/img-hosting-bucket/LSUN_256.hdf5
+wget https://storage.googleapis.com/img-hosting-bucket/LSUN_BCCC_256.hdf5
+wget https://storage.googleapis.com/img-hosting-bucket/LSUN_BCC_256.hdf5
+wget https://storage.googleapis.com/img-hosting-bucket/LSUN_Cars.hdf5
